e30873e83e722fa8214f4f02a46b8684fc553508
idsantri <idsantri.mail@gmail.com>
2024-11-22 10:55:03 +0700

    add kelas condition

--- src/pages/MadrasahPage.jsx
+++ src/pages/MadrasahPage.jsx
@@ -3,7 +3,7 @@ import { apiGet } from '../api';
 import useNotify from '../hooks/use-notify';
 import Notify from '../components/Notify';
 import Loading from '../components/Loading';
-import { Accordion, Button, Card } from 'react-bootstrap';
+import { Accordion, Alert, Button, Card } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 function KelasPage() {
@@ -27,7 +27,6 @@ function KelasPage() {
 
 	return (
 		<div>
-			<Notify {...objNotify} />
 			<div className='bg-color3'>
 				<h2
 					style={{
@@ -40,36 +39,40 @@ function KelasPage() {
 					Riwayat Kelas
 				</h2>
 			</div>
-			{kelas ? (
-				<>
-					<Card className='my-2 bg-color2 text-center'>
-						<Card.Body className='p-2 fw-bold'>{santri.nama}</Card.Body>
-					</Card>
-					<Accordion className='mt-2'>
-						{kelas.map((k) => (
-							<Accordion.Item eventKey={k.id} key={k.id}>
-								<Accordion.Header className=''>
-									<div className='fw-light'>
-										{k.th_ajaran_h} | {k.tingkat_id} | {k.kelas}
-									</div>
-								</Accordion.Header>
-								<Accordion.Body className='d-flex gap-2 justify-content-start p-2'>
-									<Button className='' as={Link} to='/kelas/id/nilai' variant='outline-primary'>
-										Nilai
-									</Button>
-									<Button className='' as={Link} to='/kelas/id/absensi' variant='outline-success'>
-										Absensi
-									</Button>
-								</Accordion.Body>
-							</Accordion.Item>
-						))}
-					</Accordion>
 
-					{/* <pre className='mt-3'>{JSON.stringify(kelas, null, 2)}</pre> */}
-				</>
-			) : (
-				<Loading />
-			)}
+			<Card className='my-2 bg-color2 text-center'>
+				<Card.Body className='p-2 fw-bold'>{santri.nama}</Card.Body>
+			</Card>
+			<Notify {...objNotify} />
+			<Accordion className='mt-2'>
+				{!kelas ? (
+					<Loading />
+				) : kelas.length == 0 ? (
+					<Alert variant='danger' className='text-center m-0'>
+						Yang bersangkutan belum memiliki riwayat kelas!
+					</Alert>
+				) : (
+					kelas.map((k) => (
+						<Accordion.Item eventKey={k.id} key={k.id}>
+							<Accordion.Header className=''>
+								<div className='fw-light'>
+									{k.th_ajaran_h} | {k.tingkat_id} | {k.kelas}
+								</div>
+							</Accordion.Header>
+							<Accordion.Body className='d-flex gap-2 justify-content-start p-2'>
+								<Button className='' as={Link} to='/kelas/id/nilai' variant='outline-primary'>
+									Nilai
+								</Button>
+								<Button className='' as={Link} to='/kelas/id/absensi' variant='outline-success'>
+									Absensi
+								</Button>
+							</Accordion.Body>
+						</Accordion.Item>
+					))
+				)}
+			</Accordion>
+
+			{/* <pre className='mt-3'>{JSON.stringify(kelas, null, 2)}</pre> */}
 		</div>
 	);
 }
