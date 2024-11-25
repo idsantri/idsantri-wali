fac298072ec7bf6fce3c09aefc68374d6cfcaa3a
idsantri <idsantri.mail@gmail.com>
2024-11-25 10:36:29 +0700

    .

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -1,11 +1,11 @@
-import { Card } from 'react-bootstrap';
 import { Outlet } from 'react-router-dom';
 import Pwa from '../components/PWA';
+import { Card, Grid2 } from '@mui/material';
 
 const AuthLayout = () => {
 	return (
-		<div style={{ margin: '20px' }}>
-			<div className='mx-auto' style={{ maxWidth: '500px' }}>
+		<Grid2 container>
+			<Grid2 display='flex' justifyContent='center' alignItems='center' size='grow'>
 				<Pwa />
 				<Card
 					style={{
@@ -13,7 +13,8 @@ const AuthLayout = () => {
 						padding: '20px',
 						textAlign: 'center',
 					}}
-					className='bg-color2 text-color7'
+					// className='bg-color2 text-color7'
+					classes={{ root: 'bg-color2 text-color7' }}
 				>
 					<header>
 						<h1 style={{ fontSize: '1.8em', fontWeight: 300 }}>Aplikasi Wali Santri</h1>
@@ -27,8 +28,8 @@ const AuthLayout = () => {
 						<Outlet />
 					</main>
 				</Card>
-			</div>
-		</div>
+			</Grid2>
+		</Grid2>
 	);
 };
 
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -5,6 +5,7 @@ import { apiPost } from '../api';
 import { Button, FloatingLabel, Form, Spinner } from 'react-bootstrap';
 import Notify from '../components/Notify';
 import { Icon } from '@iconify/react/dist/iconify.js';
+import { TextField } from '@mui/material';
 
 const Login = () => {
 	const navigate = useNavigate();
@@ -66,33 +67,34 @@ const Login = () => {
 	};
 
 	if (auth.isAuthenticated) {
-		return <Navigate to="/" />;
+		return <Navigate to='/' />;
 	}
 	return (
 		<>
 			<Notify {...objNotify} />
 			<h2 style={{ fontSize: '1.5em', marginBottom: '20px' }}>Login</h2>
-			<form action="" onSubmit={handleLogin}>
-				<FloatingLabel controlId="floatingPassword" label="ID Santri" className="mb-3">
+			<form action='' onSubmit={handleLogin}>
+				{/* <FloatingLabel controlId="floatingPassword" label="ID Santri" className="mb-3">
 					<Form.Control name="santri_id" type="text" placeholder="" required />
+				</FloatingLabel> */}
+				<TextField id='outlined-basic' label='ID Santri' name='santri_id' variant='outlined' />
+				<FloatingLabel controlId='floatingInput' label='NIK Santri' className='mb-3'>
+					<Form.Control name='nik' type='text' placeholder='' required />
 				</FloatingLabel>
-				<FloatingLabel controlId="floatingInput" label="NIK Santri" className="mb-3">
-					<Form.Control name="nik" type="text" placeholder="" required />
-				</FloatingLabel>
-				<Button type="submit" variant="outline-primary" style={{ width: '100%' }} disabled={loading}>
+				<Button type='submit' variant='outline-primary' style={{ width: '100%' }} disabled={loading}>
 					{loading ? (
 						<>
-							<Spinner animation="border" role="status" size="sm" /> {'Tunggu sebentar …'}
+							<Spinner animation='border' role='status' size='sm' /> {'Tunggu sebentar …'}
 						</>
 					) : (
 						'Login'
 					)}
 				</Button>
 			</form>
-			<p className="m-0 mt-3">
-				<a target="_blank" className="btn btn-outline-secondary d-flex justify-content-center text-color7" href="https://wa.me/6285259787553">
+			<p className='m-0 mt-3'>
+				<a target='_blank' className='btn btn-outline-secondary d-flex justify-content-center text-color7' href='https://wa.me/6285259787553'>
 					Tidak Bisa Login? Hubungi Pengurus…!
-					<Icon className="ms-2" icon="logos:whatsapp-icon" width="1.5em" height="1.5em" />
+					<Icon className='ms-2' icon='logos:whatsapp-icon' width='1.5em' height='1.5em' />
 				</a>
 			</p>
 		</>
