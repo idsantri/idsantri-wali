34a522e8f148737d7cf8c122bc0b69894b7bfee3
idsantri <idsantri.mail@gmail.com>
2024-11-28 14:55:32 +0700

    add phone

--- src/config/index.js
+++ src/config/index.js
@@ -5,6 +5,7 @@ const config = {
 	BASE_API: 'https://demo.idsantri.my.id',
 	END_API: '/wali',
 	DEV: true,
+	PHONE: '6285259787553',
 	PWA_NAME: 'Aplikasi Wali Santri (Demo)',
 	PWA_SHORT_NAME: 'Wali Santri',
 };
