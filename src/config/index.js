b583b0ae85c5b3dae02e1a429b35dde84b0515b3
idsantri <idsantri.mail@gmail.com>
2024-11-28 12:40:24 +0700

    locale dev

--- src/config/index.js
+++ src/config/index.js
@@ -2,7 +2,7 @@ const config = {
 	INS_DESC: 'Aplikasi Wali',
 	INS_NAME: 'Pondok Pesantren',
 	INS_SHORT: 'Demo Dev Aplikasi',
-	BASE_API: 'https://demo.idsantri.my.id',
+	BASE_API: 'http://localhost:8000',
 	END_API: '/wali',
 	DEV: true,
 };
