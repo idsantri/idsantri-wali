43675f59c778b43c084088f94095c3ae55a6c474
idsantri <idsantri.mail@gmail.com>
2024-12-08 14:03:32 +0700

    config

--- src/config/index.js
+++ src/config/index.js
@@ -1,12 +1,12 @@
 const config = {
 	APP_NAME: 'Aplikasi Wali Santri',
 	INS_DESC: 'Pondok Pesantren',
-	INS_NAME: 'Demo Aplikasi (Lokal)',
-	BASE_API: 'http://localhost:8000',
+	INS_NAME: 'Demo Aplikasi',
+	BASE_API: 'https://demo.idsantri.my.id',
 	END_API: '/wali',
 	DEV: true,
 	PHONE: '6285259787553',
-	PWA_NAME: 'Aplikasi Wali Santri (Lokal)',
+	PWA_NAME: 'Aplikasi Wali Santri (Demo)',
 	PWA_SHORT_NAME: 'Wali Santri',
 };
 export default config;
