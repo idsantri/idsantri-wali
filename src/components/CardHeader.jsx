73ab6dad3fbe2e001cfb51a77cb497842c6e0522
idsantri <idsantri.mail@gmail.com>
2026-03-09 03:32:03 +0700

    Refactor AlertNotFound and CardHeader components: enhance styling for better layout and consistency, update imports, and improve loading state handling in Santri page

--- src/components/CardHeader.jsx
+++ src/components/CardHeader.jsx
@@ -1,13 +1,11 @@
-// import PropTypes from 'prop-types';
-
-function CardHeader({ title }) {
+export default function CardHeader({ title }) {
 	const santri = JSON.parse(localStorage.getItem('santri') || 'null');
 
 	return (
-		<div className='mb-2 overflow-hidden border rounded-md border-jingga-300'>
-			<h2 className='p-2 m-0 text-xl font-light text-center bg-jingga-100 text-jingga-800'>{title}</h2>
+		<div className='mb-2 overflow-hidden border rounded-md border-base-300'>
+			<h2 className='p-2 m-0 font-light text-center bg-secondary text-secondary-content'>{title}</h2>
 			{santri && (
-				<div className='text-center bg-jingga-200'>
+				<div className='text-center bg-base-300'>
 					<div className='p-2'>
 						<div>{santri.nama}</div>
 						<div className='text-xs font-light'>{santri.data_akhir}</div>
@@ -17,7 +15,3 @@ function CardHeader({ title }) {
 		</div>
 	);
 }
-// CardHeader.propTypes = {
-// 	title: PropTypes.string.isRequired,
-// };
-export default CardHeader;
