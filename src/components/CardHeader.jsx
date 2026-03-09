2bee6dc64d59a64f269109fa2b387b7f28781f61
idsantri <idsantri.mail@gmail.com>
2026-03-09 20:38:12 +0700

    kelas page

--- src/components/CardHeader.jsx
+++ src/components/CardHeader.jsx
@@ -2,10 +2,10 @@ export default function CardHeader({ title }) {
 	const santri = JSON.parse(localStorage.getItem('santri') || 'null');
 
 	return (
-		<div className='mb-2 overflow-hidden border rounded-md border-base-300'>
-			<h2 className='p-2 m-0 font-light text-center bg-accent'>{title}</h2>
+		<div className='mb-2 overflow-hidden rounded-md'>
+			<h2 className='p-2 m-0 font-light text-center bg-accent text-accent-content'>{title}</h2>
 			{santri && (
-				<div className='text-center bg-base-300'>
+				<div className='text-center bg-info text-info-content'>
 					<div className='p-2'>
 						<div>{santri.nama}</div>
 						<div className='text-xs font-light'>{santri.data_akhir}</div>
--- src/components/CardKelas.jsx
+++ src/components/CardKelas.jsx
@@ -1,7 +1,7 @@
 export default function CardKelas({ data, className }) {
 	return (
-		<div className={`${className || null} w-full border rounded-md border-jingga-200 bg-jingga-100`}>
-			<div className='px-2 py-4 text-center text-jingga-800 bg-jingga-300'>
+		<div className={`${className || null} w-full rounded-md bg-success text-success-content`}>
+			<div className='px-2 py-2 text-center'>
 				{data.th_ajaran_h} | {data.tingkat} | {data.kelas}
 			</div>
 		</div>
