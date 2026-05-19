825bf1bbc234b0f8808113869294e5207b21e244
idsantri <idsantri.mail@gmail.com>
2026-05-19 16:16:29 +0700

    refactor: standardize JSX formatting and improve component structure
    - Updated LoadingAbsolute and LoadingFixed components to use single quotes for class names.
    - Simplified InstallPwa component by removing unnecessary line breaks and improving readability.
    - Refactored PesantrenPage to extract RenderItem and RenderDomisili components for better separation of concerns.
    - Moved groupByThAjaranH function inside IuranPage for better encapsulation.
    - Enhanced KelasPage by extracting RenderItem for improved readability and state management.
    - Refactored Santri component to extract RenderSantri for better structure and clarity.
    - Improved TatibPage by extracting Header, RenderRow, and RenderTable components for better organization.
    - Cleaned up format-date.js by removing redundant variable initializations and improving export statements.

--- src/layouts/_components/InstallPwa.jsx
+++ src/layouts/_components/InstallPwa.jsx
@@ -16,10 +16,7 @@ function InstallPwa() {
 			{ regex: /mac/i, name: 'Mac/iOS' },
 		];
 
-		return (
-			osList.find(({ regex }) => regex.test(userAgent))?.name ||
-			'Unknown OS'
-		);
+		return osList.find(({ regex }) => regex.test(userAgent))?.name || 'Unknown OS';
 	};
 	const isIos = () => getOS() === 'iOS';
 	const isAndroid = () => getOS() === 'Android';
@@ -37,16 +34,10 @@ function InstallPwa() {
 			}
 		};
 
-		window.addEventListener(
-			'beforeinstallprompt',
-			handleBeforeInstallPrompt,
-		);
+		window.addEventListener('beforeinstallprompt', handleBeforeInstallPrompt);
 
 		return () => {
-			window.removeEventListener(
-				'beforeinstallprompt',
-				handleBeforeInstallPrompt,
-			);
+			window.removeEventListener('beforeinstallprompt', handleBeforeInstallPrompt);
 		};
 	});
 
@@ -72,90 +63,60 @@ function InstallPwa() {
 		<>
 			{showInstallBanner && (
 				<>
-					<div
-						role="alert"
-						className="p-2 mb-2 rounded-md card bg-success text-success-content"
-					>
-						<div className="flex items-center justify-between gap-2">
-							<div className="flex items-center gap-2">
+					<div role='alert' className='p-2 mb-2 rounded-md card bg-success text-success-content'>
+						<div className='flex items-center justify-between gap-2'>
+							<div className='flex items-center gap-2'>
 								<div>
-									<Icon
-										icon="typcn:info"
-										height="32"
-										className=""
-									/>
+									<Icon icon='typcn:info' height='32' className='' />
 								</div>
-								<div className="text-sm italic text-left">
-									Instal aplikasi untuk mendapatkan pengalaman
-									lebih baik!
+								<div className='text-sm italic text-left'>
+									Instal aplikasi untuk mendapatkan pengalaman lebih baik!
 								</div>
 							</div>
 
 							{isAndroid() && (
-								<button
-									className="px-2 btn btn-neutral "
-									onClick={installPwa}
-								>
-									<Icon
-										icon="material-symbols-light:install-mobile"
-										height="24"
-									/>
+								<button className='px-2 btn btn-neutral ' onClick={installPwa}>
+									<Icon icon='material-symbols-light:install-mobile' height='24' />
 									Instal
 								</button>
 							)}
 							{isIos() && (
 								<button
-									className="font-medium border-none rounded-full shadow-sm btn btn-ghost btn-circle btn-sm text-neutral shadow-neutral"
+									className='font-medium border-none rounded-full shadow-sm btn btn-ghost btn-circle btn-sm text-neutral shadow-neutral'
 									onClick={() => setShowIosComp(!showIosComp)}
 								>
 									{showIosComp ? (
-										<Icon
-											icon="mingcute:up-fill"
-											width="24"
-											height="24"
-										/>
+										<Icon icon='mingcute:up-fill' width='24' height='24' />
 									) : (
-										<Icon
-											icon="mingcute:down-fill"
-											width="24"
-											height="24"
-										/>
+										<Icon icon='mingcute:down-fill' width='24' height='24' />
 									)}
 								</button>
 							)}
 						</div>
 						{showIosComp && (
 							<>
-								<div className="mt-2 mb-0 divider"></div>
-								<div className="w-full text-left rounded-md card ">
+								<div className='mt-2 mb-0 divider'></div>
+								<div className='w-full text-left rounded-md card '>
 									<p>Bagi Pengguna iOS!</p>
-									<ul className="ml-4 text-sm font-light list-decimal list-outside">
-										<li className="">
+									<ul className='ml-4 text-sm font-light list-decimal list-outside'>
+										<li className=''>
 											Pastikan Anda menggunakan browser{' '}
-											<span className="font-semibold">
-												Safari
-											</span>
-											;
+											<span className='font-semibold'>Safari</span>;
 										</li>
-										<li className="">
-											Ketuk ikon bagikan (sebuah kotak
-											dengan panah ke atas di bagian bawah
+										<li className=''>
+											Ketuk ikon bagikan (sebuah kotak dengan panah ke atas di bagian bawah
 											layar);
 										</li>
 										<li>
-											Gulir ke bawah dalam menu bagikan
-											dan pilih opsi &ldquo;Tambahkan ke
-											Layar Utama&rdquo;;
+											Gulir ke bawah dalam menu bagikan dan pilih opsi &ldquo;Tambahkan ke Layar
+											Utama&rdquo;;
 										</li>
 										<li>
-											Anda mungkin akan diminta untuk
-											memberikan nama dan mengonfigurasi
-											ikon;
+											Anda mungkin akan diminta untuk memberikan nama dan mengonfigurasi ikon;
 										</li>
 										<li>
-											Ketuk &ldquo;Tambahkan&rdquo; di
-											pojok kanan atas layar untuk
-											menyelesaikan proses;
+											Ketuk &ldquo;Tambahkan&rdquo; di pojok kanan atas layar untuk menyelesaikan
+											proses;
 										</li>
 									</ul>
 								</div>
--- src/pages/domisili/Index.jsx
+++ src/pages/domisili/Index.jsx
@@ -5,12 +5,42 @@ import AlertNotFound from '../../components/AlertNotFound';
 import LoadingAbsolute from '../../components/LoadingAbsolute';
 import { getDomisili } from '../../models/santri';
 
+function RenderItem({ dom, index: i }) {
+	return (
+		<div className={`flex items-center justify-between px-3 py-1 ${i == 0 ? '' : 'border-t border-accent/50'}`}>
+			<div className='font-light'>
+				<div className=''>
+					<span className='inline-block text-sm italic w-15'>Kamar:</span>
+					<span className=''>{dom.domisili}</span>
+				</div>
+				<div className=''>
+					<span className='inline-block text-sm italic w-15'>Ket.:</span>
+					<span>{dom.keterangan || '-'}</span>
+				</div>
+			</div>
+			<div className='text-xs text-right'>
+				<div>{format(addHours(dom.cr_at, 7), 'yyyy-MM-dd')}</div>
+				{/* <div>{addHours(dom.cr_at, 7).toString().slice(11, 16)}</div> */}
+			</div>
+		</div>
+	);
+}
+
+function RenderDomisili({ domisili }) {
+	return (
+		<div className='overflow-hidden border rounded-md border-accent/75'>
+			{domisili.map((dom, i) => (
+				<RenderItem key={i} dom={dom} index={i} />
+			))}
+		</div>
+	);
+}
+
 function PesantrenPage() {
 	const [domisili, setDomisili] = useState(null);
-	const [isLoading, setIsLoading] = useState(false);
+	const [isLoading, setIsLoading] = useState(true);
 
 	useEffect(() => {
-		setIsLoading(true);
 		getDomisili()
 			.then((data) => {
 				if (data && data.domisili) {
@@ -19,37 +49,6 @@ function PesantrenPage() {
 			})
 			.finally(() => setIsLoading(false));
 	}, []);
-
-	function RenderItem({ dom, index: i }) {
-		return (
-			<div className={`flex items-center justify-between px-3 py-1 ${i == 0 ? '' : 'border-t border-accent/50'}`}>
-				<div className='font-light'>
-					<div className=''>
-						<span className='inline-block text-sm italic w-15'>Kamar:</span>
-						<span className=''>{dom.domisili}</span>
-					</div>
-					<div className=''>
-						<span className='inline-block text-sm italic w-15'>Ket.:</span>
-						<span>{dom.keterangan || '-'}</span>
-					</div>
-				</div>
-				<div className='text-xs text-right'>
-					<div>{format(addHours(dom.cr_at, 7), 'yyyy-MM-dd')}</div>
-					{/* <div>{addHours(dom.cr_at, 7).toString().slice(11, 16)}</div> */}
-				</div>
-			</div>
-		);
-	}
-
-	function RenderDomisili({ domisili }) {
-		return (
-			<div className='overflow-hidden border rounded-md border-accent/75'>
-				{domisili.map((dom, i) => (
-					<RenderItem key={i} dom={dom} index={i} />
-				))}
-			</div>
-		);
-	}
 
 	return (
 		<>
