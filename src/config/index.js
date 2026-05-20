215aaf91289d58310490d6007bc6e0fa17cb26bc
idsantri <idsantri.mail@gmail.com>
2026-05-20 09:51:45 +0700

    fix: update application configuration for demo environment

--- src/config/index.js
+++ src/config/index.js
@@ -1,11 +1,11 @@
 const config = {
 	APP_NAME: 'Aplikasi Wali Santri',
 	INS_DESC: 'Pondok Pesantren',
-	INS_NAME: 'Demo Aplikasi (Lokal)',
-	BASE_API: 'http://localhost:8000',
+	INS_NAME: 'Demo Aplikasi',
+	BASE_API: 'https://demo.idsantri.my.id',
 	END_API: '/wali/',
 	DEV: true,
-	PWA_NAME: 'Aplikasi Wali Santri (Lokal)',
+	PWA_NAME: 'Aplikasi Wali Santri (Demo)',
 	PWA_SHORT_NAME: 'Wali Santri',
 };
 export default config;
