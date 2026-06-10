d61f7925a920e0e149d8684b609b821f6d59745b
idsantri <idsantri.mail@gmail.com>
2026-06-10 13:52:02 +0700

    fix: revert BASE_API URL to local development address

--- src/config/index.js
+++ src/config/index.js
@@ -2,7 +2,7 @@ const config = {
 	APP_NAME: 'Aplikasi Wali Santri',
 	INS_DESC: 'Pondok Pesantren',
 	INS_NAME: 'Demo Aplikasi',
-	BASE_API: 'https://demo.idsantri.my.id',
+	BASE_API: 'http://127.0.0.1:8000',
 	END_API: '/wali/',
 	DEV: true,
 	PWA_NAME: 'Aplikasi Wali Santri (Demo)',
